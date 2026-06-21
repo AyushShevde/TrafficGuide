fdaa1f94870bdf35e8ab884c5f98b1290ae40372
AyushShevde <ayush.shevde7@gmail.com>
2026-06-21 15:00:39 +0530

    updated backend

--- backend/api/main.py
+++ backend/api/main.py
@@ -135,6 +135,7 @@ app.add_middleware(
         "http://127.0.0.1",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:5173",
+        "https://traffic-guide-996d.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
